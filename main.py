4a1a7b009f872171bff468cb1deb1c4a020fc0dd
T-r-666 <1154730956@qq.com>
2025-10-25 19:28:49 +0800

    完成实现 isOdd() 函数的作业

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # 在此文件中实现 isOdd 函数
-
-def isOdd(value):
+   def isOdd(value):
     """
     判断输入是否为奇整数    
     参数:
@@ -8,6 +7,6 @@ def isOdd(value):
     返回:
     bool - 如果是整数且为奇数返回 True，否则返回 False
     """
-    # 学生实现代码区域
-    # 提示：首先检查类型是否为整数，然后检查奇偶性
-    
+    if type(value) is int:
+        return value % 2 != 0
+    return False
